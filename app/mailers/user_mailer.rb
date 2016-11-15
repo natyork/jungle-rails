bd8db4ad8581f2d209e1b0fa38b6831de1ba2b60
Nat York <natyork@gmail.com>
2016-11-15 04:20:15 +0000

    Clear mail deliveries after action

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,5 +1,6 @@
 class UserMailer < ApplicationMailer
-  after_action :set_delivery_options
+  after_action  :set_delivery_options
+                :log_and_clear_emails
 
   def welcome_email(order)
       @order = order
@@ -12,5 +13,9 @@ class UserMailer < ApplicationMailer
 
     def set_delivery_options
         mail.delivery_method :test
+    end
 
+    def log_and_clear_emails
+        mail.deliveries.each { |d| Rails.logger.debug(d) }
+        mail.deliveries.clear
     end
