e19c8d3d328841db852843251e8ffcca38802bb5
Nat York <natyork@gmail.com>
2016-11-15 03:30:07 +0000

    Remove mailer updates.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,13 +3,13 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  after_action :log_and_clear_emails
-  def log_and_clear_emails
-    if ActiveMailer::Base.delivery_method == :test
-      ActiveMailer::Base.deliveries.each { |d| Rails.logger.debug(d) }
-      ActiveMailer::Base.deliveries.clear
-    end
-  end
+  # after_action :log_and_clear_emails
+  # def log_and_clear_emails
+  #   if ActiveMailer::Base.delivery_method == :test
+  #     ActiveMailer::Base.deliveries.each { |d| Rails.logger.debug(d) }
+  #     ActiveMailer::Base.deliveries.clear
+  #   end
+  # end
 
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
